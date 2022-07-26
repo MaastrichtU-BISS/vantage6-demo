5ca4175a7fc6c66a12433224d0c348c32a44c86c
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2022-07-26 20:30:37 +0200

    Scaffolding the infrastructure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# beweeghuis-pht
+# Beweeghuis Personal Health Train
 Proof of principle repository for the Beweeghuis project
---
+++ infrastructure/setup.ps1
@@ -0,0 +1,22 @@
+python -m venv .\venv
+.\venv\Scripts\activate
+
+pip install -r requirements.txt
+
+# Technically this is not needed, but makes life more easy ;-)
+#docker pull harbor2.vantage6.ai/infrastructure/server:petronas
+docker pull harbor2.vantage6.ai/infrastructure/server:3.2.0
+#docker pull harbor2.vantage6.ai/infrastructure/node:petronas
+docker pull harbor2.vantage6.ai/infrastructure/node:3.2.0
+
+# Start server
+$server_config = $(Convert-Path -path $(Resolve-Path beweeghuis.yaml))
+vserver start -c $server_config
+
+# Import server entities
+$server_entities = $(Convert-Path -path $(Resolve-Path entities.yaml))
+vserver import -c $server_config $server_entities
+
+# Fysio node start
+$fysio_config = $(Convert-Path -path $(Resolve-Path fysio.yaml))
+vnode start -c $fysio_config
