cea211790287aa32a847f96039678bd171ccb2c2
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2024-05-29 12:37:56 +0200

    updated teardown scripts

--- infrastructure/shutdown.sh
+++ infrastructure/shutdown.sh
@@ -1,7 +1,7 @@
 source ./venv/bin/activate
-vnode stop --user -n ortho
-vnode stop --user -n fysio
-vserver stop --user -n beweeghuis
+v6 node stop --user -n ortho
+v6 node stop --user -n fysio
+v6 server stop --user -n beweeghuis
 deactivate
 
 docker volume rm $(docker volume ls -f name=vantage6 -q)
@@ -11,10 +11,12 @@ docker volume rm $(docker volume ls -f name=fysio -q)
 # Optional: delete images
 # docker rmi -f $(docker image ls --filter=reference='harbor*/*/*' -q)
 
-#macOS
-rm -R ~/Library/Application\ Support/vantage6/node/fysio
-rm -R ~/Library/Application\ Support/vantage6/node/ortho
-rm -R ~/Library/Application\ Support/vantage6/server/beweeghuis
+# #macOS
+# rm -R ~/Library/Application\ Support/vantage6/node/fysio
+# rm -R ~/Library/Application\ Support/vantage6/node/ortho
+# rm -R ~/Library/Application\ Support/vantage6/server/beweeghuis
 
 # Linux
+rm -Rf ~/.local/share/vantage6/node/fysio
+rm -Rf ~/.local/share/vantage6/node/ortho
 rm -Rf ~/.local/share/vantage6/server/beweeghuis
